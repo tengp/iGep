d3b9db26cdbdec5e54c69cfeb2a83c2f56c8d1c2
tengp <teng1029@gmail.com>
2015-03-12 21:05:06 -0400

    Delete print.py

--- print.py
+++
@@ -1,9 +0,0 @@
-import sys,os
-
-def printprint(a,b,c,d):
-        
- 
-        # return a,b,c,d
-        print a+"<br>"
-        print b+"<br>"
-printprint(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4])
